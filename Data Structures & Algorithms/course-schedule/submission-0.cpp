f8a8c021d7741cd3199556e51f03a359586f769b
Khushi Tulsiyan <khushitulsiyan495@gmail.com>
2026-05-31 22:38:02 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    unordered_map<int, vector<int>> prepMap;
+    unordered_set<int> visiting;
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        for(int i =- 0; i<numCourses; i++){
+            prepMap[i] = {};
+        }
+        for(const auto &prereq : prerequisites){
+            prepMap[prereq[0]].push_back(prereq[1]);
+        }
+
+        for(int c= 0; c< numCourses; c++){
+            if(!dfs(c)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool dfs(int crs){
+        if(visiting.count(crs)){
+            return false;
+        }
+        if(prepMap[crs].empty()){
+            return true;
+        }
+
+        visiting.insert(crs);
+        for(int pre: prepMap[crs]){
+            if(!dfs(pre)){
+                return false;
+            }
+        }
+        visiting.erase(crs);
+        prepMap[crs].clear();
+        return true;
+    }
+};
